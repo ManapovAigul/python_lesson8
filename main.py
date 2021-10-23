f8f03adf77727b5609bdd5f82a49915e5ecb525c
ManapovAigul <brave-girl.90@mail.ru>
2021-10-23 20:12:46 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def divide(x=12345678, y=2):
+    return x / y
+
+def multiple(x, y=4):
+    return x * y
+
+def add(x, y=24691356.0):
+    return x - y
+
+if __name__ == '__main__':
+    print(divide())
+    print(multiple(6172839.0))
+    print(add(24691354.0))
+    
+
